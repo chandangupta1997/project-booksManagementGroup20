5ea5385a28bb07efb4a20dd88cd4261c3eba8762
chandangupta1997 <chandangupta8000@gmail.com>
2022-03-25 15:41:10 +0530

    ok

--- src/validations/validator.js
+++ src/validations/validator.js
@@ -1,4 +1,4 @@
-
+const mongoose = require('mongoose');
 
 
 const isValid =function(value){
@@ -25,3 +25,8 @@ const isValidRequestBody=function(requestBody){
 }
 
 //this is a change
+//this is second change 
+
+
+
+module.exports={isValid,isValidObjectId,isValidRequestBody}
