09c7a1077f882101ad93a73e0a69fa0f8cde1d6e
chandangupta1997 <chandangupta8000@gmail.com>
2022-03-25 18:26:12 +0530

    terimeri

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,8 +1,40 @@
 const bookModel =require("../models/bookModel")
 const reviewModel=require("../models/reviewModel")
 const userModel =require("../models/userModel")
+const validations =require("../validations/validator")
 
 
 
 
+const createBook = async function(req,res){
+    
 
+    try{
+
+        let data = req.body 
+    if(!validations.isValidRequestBody(data)){
+        res.status(400).send({status:"false",msg:"body should not be empty "})
+        return
+    }
+
+    const {title,excerpt,userId,ISBN,category,subcategory,reviews}=data
+
+    //validation starts
+
+    //validation finish
+
+   // const bookData={title,excerpt,userId,ISBN,category,} // accessing it for blog Creation
+
+   //later on we will destructure data 
+
+    const newBook = await bookSchema.create(data)
+    res.send(newBook)
+    
+
+    }
+
+    catch(error){
+
+        res.status(500).send({status:"false",msg:error.message})
+    }
+}
---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const bookController=require("../controllers/bookController")
+const reviewController=require("../controllers/reviewController")
+const userController=require("../controllers/userController")
+
+
+//createBook 
+router.post("/createBook",bookController.createBook)
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
