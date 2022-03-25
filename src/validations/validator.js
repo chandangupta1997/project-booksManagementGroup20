b6a36f5a5429cf3307bc9f435e76921fb88dd23e
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2022-03-25 22:30:42 +0530

    Update validator.js

--- src/validations/validator.js
+++ src/validations/validator.js
@@ -5,19 +5,10 @@ const isValid =function(value){
     if(typeof value ==='undefined'|| value===null) return false 
     if(typeof value==='string' && value.trim().length===0) return false 
     return true
-   
-
-
-
-}
-
-
+   }
 
 const isValidObjectId=function(objectId){
-    return mongoose.Types.ObjectId.isvalid(objectId)
-    
-    
-    
+    return mongoose.Types.ObjectId.isvalid(objectId)    
 }
 
 const isValidRequestBody=function(requestBody){
