6d8802e8a4a2320bc6b1ce5c5d2e6c1ee195c697
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2022-03-29 18:15:11 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,23 +1,46 @@
-const express = require('express');
-const router = express.Router();
-const bookController=require("../controllers/bookController")
-const reviewController=require("../controllers/reviewController")
-const userController=require("../controllers/userController")
-const middleware = require("../middleware/auth")
+const jwt=require('jsonwebtoken');
+const userModel=require('../models/userModel')
+const bookModel =require('../models/bookModel');
+const verify = require('jsonwebtoken/verify');
+let authenticate= async function(req, res, next){
+    try{
+        let token=req.headers['x-api-key']
+        if(!token)
+        return res.status(400).send({status:false, message:"Please provide a valid token"})
+    let validatetoken=jwt.verify(token,"Group20")
+    if(!validatetoken)
+    return res.status(400).send({status:false, message:"Authentication failed"})
+    next()
+    }catch(err){
+        console.log("This is the error:",err.message)
+        res.status(500).send({status:false,error:err.message})
+    }
+}
+let authorise=async function(req, res, next){
+    try{
+    let bookId=req.params.bookId
 
-//createBook 
-router.post("/createBook",bookController.createBook)
-router.get("/getBook",bookController.getBook)
-router.get("/book/:bookId",middleware.authorise,bookController.getBooksById)
-
-router.put("/book/:bookId",middleware.authorise,bookController.updateBookById)
-router.delete("/book/:bookId",middleware.authorise,bookController.deleteById)
-
-//createUser
-router.post("/createUser",userController.createUser)
-router.post("/loginUser",userController.loginUser)
-
-// adding Review 
-//router.post("/addReview/:bookId/review",reviewContrroller.addReview)
-
-module.exports = router;
+    let jwtToken=req.headers['x-api-key']
+    
+    if(!jwtToken)
+    return res.status(400).send({status:false, message:"Please provide a valid token"})
+        
+        let verifiedToken=jwt.verify(jwtToken,"Group20")
+        let decodedId = verifiedToken.userId
+        console.log(decodedId)
+        
+    
+        let findBook = await bookModel.findById(bookId)
+        let userId = findBook.userId
+        if (userId != decodedId)
+        {
+           return res.status(400).send({status:false,message:"You are not to get data"})
+        }
+         next()
+    }catch(err){
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
+}
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
