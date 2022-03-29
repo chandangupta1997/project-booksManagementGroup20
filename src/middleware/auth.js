ab6372b46e2689ff3e5bff7c675d3280a4f7fa62
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2022-03-29 18:14:23 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,37 +1,23 @@
-const bookModel =require("../models/bookModel")
-const reviewModel=require("../models/reviewModel")
-const userModel =require("../models/userModel")
+const express = require('express');
+const router = express.Router();
+const bookController=require("../controllers/bookController")
+const reviewController=require("../controllers/reviewController")
+const userController=require("../controllers/userController")
+const middleware = require("../middleware/auth")
 
-const jwt=require('jsonwebtoken');
-const userModel=require('../models/userModel')
-let authenticate= async function(req, res, next){
-    try{
-        let token=req.headers['x-api-key']
-        if(!token)
-        return res.status(400).send({status:false, message:"Please provide a valid token"})
-    let validatetoken=jwt.verify(token,"Group20")
-    if(!validatetoken)
-    return res.status(400).send({status:false, message:"Authentication failed"})
-    next()
-    }catch(err){
-        console.log("This is the error:",err.message)
-        res.status(500).send({status:false,error:err.message})
-    }
-}
-let authorise=async function(req, res, next){
-    let useId=req.params.userId
-    let jwtToken=req.headers['x-api-key']
-    try{
-        let verifiedToken=jwt.verify(jwtToken,"Group20")
-        if(verifiedToken.userId=useId)
-        return res.status(403).send({status:false,message:"Unauthorised access denied"})
-        next()
-    }catch(err){
-        console.log("This is the error :", err.message)
-        res.status(500).send({ msg: "Error", error: err.message })
-    }
-}
-module.exports.authenticate = authenticate
-module.exports.authorise = authorise
+//createBook 
+router.post("/createBook",bookController.createBook)
+router.get("/getBook",bookController.getBook)
+router.get("/book/:bookId",middleware.authorise,bookController.getBooksById)
 
+router.put("/book/:bookId",middleware.authorise,bookController.updateBookById)
+router.delete("/book/:bookId",middleware.authorise,bookController.deleteById)
 
+//createUser
+router.post("/createUser",userController.createUser)
+router.post("/loginUser",userController.loginUser)
+
+// adding Review 
+//router.post("/addReview/:bookId/review",reviewContrroller.addReview)
+
+module.exports = router;
