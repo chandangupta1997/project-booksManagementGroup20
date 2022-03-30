281ff3848b3524411ec729402b3617e23054f274
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2022-03-31 00:10:03 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,46 +1,37 @@
-const jwt=require('jsonwebtoken');
-const userModel=require('../models/userModel')
-const bookModel =require('../models/bookModel');
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/userModel')
+const bookModel = require('../models/bookModel');
+const validations = require('../validations/validator.js');
 const verify = require('jsonwebtoken/verify');
-let authenticate= async function(req, res, next){
-    try{
-        let token=req.headers['x-api-key']
-        if(!token)
-        return res.status(400).send({status:false, message:"Please provide a valid token"})
-    let validatetoken=jwt.verify(token,"Group20")
-    if(!validatetoken)
-    return res.status(400).send({status:false, message:"Authentication failed"})
-    next()
-    }catch(err){
-        console.log("This is the error:",err.message)
-        res.status(500).send({status:false,error:err.message})
-    }
-}
-let authorise=async function(req, res, next){
-    try{
-    let bookId=req.params.bookId
+let authorise = async function (req, res, next) {
+    try {
 
-    let jwtToken=req.headers['x-api-key']
-    
-    if(!jwtToken)
-    return res.status(400).send({status:false, message:"Please provide a valid token"})
-        
-        let verifiedToken=jwt.verify(jwtToken,"Group20")
-        let decodedId = verifiedToken.userId
-        console.log(decodedId)
-        
-    
-        let findBook = await bookModel.findById(bookId)
-        let userId = findBook.userId
-        if (userId != decodedId)
-        {
-           return res.status(400).send({status:false,message:"You are not authorised"})
+
+        let token = req.headers["x-api-key"]
+        if (!token) { return res.status(400).send({ Status: false, msg: "Token must be present" }) }
+
+        let decodedToken = jwt.verify(token, 'Group20')
+        if (!decodedToken) { return res.status(400).send({ status: false, msg: "Invalid token id" }) }
+
+        if (userId = req.body.userId) {
+            if (decodedToken.userId != req.body.userId) return res.status(400).send({ status: false, msg: "You are not authorised user" })
         }
-         next()
-    }catch(err){
+
+        if (bookId = req.params.bookId) {
+            const bookId = req.params.bookId
+            if (!(validations.isValid(bookId) && validations.isValidObjectId(bookId))) {
+                return res.status(400).send({ status: false, msg: "BookId not valid" })
+            }
+            let book = await bookModel.findOne({ _id: req.params.bookId })
+            if (decodedToken.userId != book.userId) return res.status(400).send({ status: false, msg: "You are not authorised user " })
+
+        }
+
+        next()
+    } catch (err) {
         console.log("This is the error :", err.message)
         res.status(500).send({ msg: "Error", error: err.message })
     }
 }
-module.exports.authenticate = authenticate
+
 module.exports.authorise = authorise
