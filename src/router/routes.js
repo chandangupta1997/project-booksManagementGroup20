341a48221a2c71d41180f5c3d8ee2ccb5abdd13e
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2022-03-31 00:11:51 +0530

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,19 +5,21 @@ const reviewController=require("../controllers/reviewController")
 const userController=require("../controllers/userController")
 const middleware = require("../middleware/auth")
 
-//createBook 
-router.post("/createBook",bookController.createBook)
-router.get("/getBook",bookController.getBook)
-router.get("/books/:bookId",bookController.getBooksById)
-
-router.put("/books/:bookId",bookController.updateBookById)
-router.delete("/books/:bookId",bookController.deleteById)
-
 //createUser
 router.post("/createUser",userController.createUser)
 router.post("/loginUser",userController.loginUser)
+//createBook 
+router.post("/createBook",middleware.authorise,bookController.createBook)
+router.get("/getBook",bookController.getBook)
+router.get("/book/:bookId",middleware.authorise,bookController.getBooksById)
+
+router.put("/book/:bookId",middleware.authorise,bookController.updateBookById)
+router.delete("/book/:bookId",middleware.authorise,bookController.deleteById)
+
+
 
 // adding Review 
-//router.post("/addReview/:bookId/review",reviewContrroller.addReview)
-
+router.post("/books/:bookId/review",reviewController.addReview)
+router.put("/books/:bookId/review/:reviewId",reviewController.updateReview)
+router.delete("/books/:bookId/review/:reviewId",reviewController.deleteReview)
 module.exports = router;
